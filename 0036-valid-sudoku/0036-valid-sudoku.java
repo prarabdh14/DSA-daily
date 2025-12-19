0b75b49392622a5f7c197ae6a2d73dbf2c6a531b
Prarabdh Atrey <117309848+prarabdh14@users.noreply.github.com>
2025-12-20 01:16:33 +0530

    Time: 14 ms (8.17%), Space: 52.4 MB (5.15%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -11,11 +11,9 @@ class Solution {
             {
                 if(board[i][j]=='.')
                 continue;
-                else if(!present_row.add(board[i][j] + "inrow" + i))
-                return false;
-                else if(!present_col.add(board[i][j] + "incol" + j))
-                return false;
-                else if(!present_grid.add(board[i][j] + "ingrid" + i/3 + j/3))
+                if((!present_row.add(board[i][j] + "inrow" + i)) ||
+                (!present_col.add(board[i][j] + "incol" + j)) ||
+                (!present_grid.add(board[i][j] + "ingrid" + i/3 + j/3))) 
                 return false;
             }
         } 
