056d6cc8209a0e2b533019bfe0f0cd700eab5ca3
Prarabdh Atrey <117309848+prarabdh14@users.noreply.github.com>
2025-12-23 19:16:31 +0530

    Time: 20 ms (70.29%), Space: 56.6 MB (15.77%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) 
+    {
+        Arrays.sort(people);
+        int n = people.length;
+        int left = 0; 
+        int right = n - 1;   
+        int boat = 0;
+        while(left <= right)
+        {
+            int sum = people[left] + people[right];
+            if(sum <= limit)
+            {
+                left++;
+            }
+            right--;
+            boat++;
+        }
+        return boat;
+    }
+}
