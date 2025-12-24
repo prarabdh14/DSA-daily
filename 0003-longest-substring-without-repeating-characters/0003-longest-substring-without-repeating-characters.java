79e4071f4d5236de3810c1f9d72d1cb92b91f88e
Prarabdh Atrey <117309848+prarabdh14@users.noreply.github.com>
2025-12-24 21:06:15 +0530

    Time: 63 ms (9.6%), Space: 46.8 MB (10.74%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -4,16 +4,18 @@ class Solution {
         int n = s.length();
         int max_len = 0;
         int i = 0;
+        HashSet<Character> present = new HashSet<>();
+
         while(i < n)
         {
             int j = i;
             //int count =0;
-            HashSet<Character> present = new HashSet<>();
             while(j < n && present.add(s.charAt(j)))
             {
                 //count++;
                 j++;
             }
+            present.clear();
             if(j-i > max_len)
             max_len = j-i;
             i++;
