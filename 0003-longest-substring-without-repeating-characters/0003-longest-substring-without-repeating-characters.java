b19f50180cc9ff9b04c995720dc44025395c4c52
Prarabdh Atrey <117309848+prarabdh14@users.noreply.github.com>
2026-03-24 22:41:36 +0530

    Time: 2 ms (98.09%), Space: 44.9 MB (91.04%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,26 +1,48 @@
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
+    public int lengthOfLongestSubstring(String s) 
+    {
+        /*
+        int  max = 0;
         int n = s.length();
-        int max_len = 0;
-        int i = 0;
-        int j = 0;
-        HashSet<Character> present = new HashSet<>();
-
-        while (j < n) {
-            //int j = i;
-            //int count =0;
-            while (present.contains(s.charAt(j))) {
-                //count++;
-                present.remove(s.charAt(i));
-                i++;
+        for(int i = 0; i < n ; i++)
+        {
+            int count = 0;
+            HashSet<Character> hash = new HashSet<>();
+            for(int j = i ; j < n ; j++)
+            {
+                if(hash.contains(s.charAt(j)))
+                {
+                    break;
+                }
+                else
+                {
+                    hash.add(s.charAt(j));
+                    count++;
+                }
             }
-            present.add(s.charAt(j));
-            if (j - i + 1 > max_len)
-                max_len = j - i + 1;
-            j++;
-            if (i > n - max_len)
-                return max_len;
+            if(count > max)
+            max = count;
+        }    
+        return max;*/
+        int  max = 0, left =0;
+        int n = s.length();
+        int arr[] = new int[256];
+        Arrays.fill(arr, -1);
+        for(int right = 0 ; right < n ; right++)
+        {
+            if(arr[s.charAt(right)] != -1)
+            {
+                if(arr[s.charAt(right)] >= left)
+                {
+                    left = arr[s.charAt(right)] + 1;
+                }
+            }
+            if((right - left + 1 ) > max)
+            {
+                max = right - left + 1;
+            }
+            arr[s.charAt(right)] = right;
         }
-        return max_len;
+        return max;
     }
 }
