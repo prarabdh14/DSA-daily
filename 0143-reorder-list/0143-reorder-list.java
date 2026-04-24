ec9e3df20293983f73f0eb93426fc2932cba42c3
Prarabdh Atrey <117309848+prarabdh14@users.noreply.github.com>
2026-04-25 03:37:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -9,26 +9,43 @@
  * }
  */
 class Solution {
-    public void reorderList(ListNode head) {
-        if (head == null || head.next == null)
-            return;
-
-        ListNode start = head;
-
-        while (start != null && start.next != null && start.next.next != null) {
-            ListNode prevToEnd = start;
-
-            while (prevToEnd.next.next != null) {
-                prevToEnd = prevToEnd.next;
-            }
-
-            ListNode end = prevToEnd.next;
-            prevToEnd.next = null;
-
-            end.next = start.next;
-            start.next = end;
-
-            start = end.next;
+    public void reorderList(ListNode head) 
+    {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
         }
+        ListNode second;
+        
+        second = reverseList(slow.next);    
+        slow.next = null;        
+        slow = head;
+        while(second != null)
+        {
+            ListNode temp = slow.next;
+            ListNode next2 = second.next;
+            slow.next = second;
+            second.next = temp;
+            slow = temp;
+            second = next2;
+       }
+
+        
+    }
+    public ListNode reverseList(ListNode head)
+    {
+        ListNode curr = head;
+        ListNode prev = null;
+        while(curr != null)
+        {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
     }
 }
